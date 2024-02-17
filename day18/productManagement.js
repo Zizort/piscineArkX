baf35875226ccac7ca03f8640001c8ef3ecc9c63
Zizort <mtbox529@gmail.com>
2024-02-17 22:09:46 +0100

    day18 finnish challenge

--- day18/productManagement.js
+++ day18/productManagement.js
@@ -24,81 +24,143 @@ app.use((req, res, next) => {
 });
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-app.get('/products', (req, res) => {
-    res.json(products)
+app.get('/products', (req, res, next) => {
+    try {
+        res.json(products)
+    } catch (error) {
+        next(error);
+    }    
 });
-app.get('/products/search', (req, res) => {
-    let a = req.query.q;
-    let b = req.query.minPrice;
-    let c = req.query.maxPrice;
-    //res.send(a + " " + b + " " + c);
-    const arr = products.filter((ele) => { return ele.price <= c && ele.price >= b});
-    //res.send(arr.map((x) => `${a}: id: ${x.id}, name: ${x.name}, price: ${x.price}\n`)); // send and json seem identical // statue code 304
-    res.json(arr);
+app.get('/products/search', (req, res, next) => {
+    try {
+        let a = req.query.q;
+        let b = req.query.minPrice;
+        let c = req.query.maxPrice;
+        //res.send(a + " " + b + " " + c);
+        const arr = products.filter((ele) => { return ele.price <= c && ele.price >= b});
+        //res.send(arr.map((x) => `${a}: id: ${x.id}, name: ${x.name}, price: ${x.price}\n`)); // send and json seem identical // statue code 304
+        if (arr) {
+            res.json(arr);
+        }
+        else {
+            // res.send("nothing");
+            const error = new Error("nothing");
+            error.status = 400;
+            next(error);
+        }
+    } catch (error) {
+        next(error);
+    }    
+
     //res.json(arr);
 });
-app.get('/products/:id', (req, res) => {
-    let wanted = req.params.id;
-    let found = products.find((ele) => ele.id == wanted);
-    if (found) {
-        res.json(found);
-    } else {
-        res.status(400).send("product not found");
-    }
+app.get('/products/:id', (req, res, next) => {
+    try {
+        let wanted = req.params.id;
+        let found = products.find((ele) => ele.id == wanted);
+        if (found) {
+            res.json(found);
+        } else {
+            //res.status(400).send("product not found");
+            const error = new Error("product not found");
+            error.status = 400;
+            next(error);
+            // return;
+        }
+    } catch (error) {
+        next(error);
+    }    
 });
 
 
 //postman
-app.post('/products', (req, res) => {
-    const data = req.body;
-    let newUser = {id: products[products.length - 1].id + 1, name: data.name, price: data.price};
-    products.push(newUser);
-    console.log(products);
-    res.json(products);
+app.post('/products', (req, res, next) => {
+    try {
+        const data = req.body;
+        let newUser = {id: products[products.length - 1].id + 1, name: data.name, price: data.price};
+        products.push(newUser);
+        console.log(products);
+        res.json(products);
+    } catch(error) {
+        next(error);
+    }
 });
 
-app.put('/products/:id', (req, res) => {
-    let ID = req.params.id;
-    //let j = 0
-    let wanted = products.find((ele) => { return ele.id == ID});//j++; 
-    if (wanted) {
-        let i = products.indexOf(wanted);
-        const data = req.body; //if data contains the whole object
-        // products[wanted] = data; //this does not work //using the object as an index in array of objects
-        if (data.name)
-            products[i].name = data.name; 
-        if (data.price)
-            products[i].price = data.price;
-        res.json(products);
-    } else {
-        res.status(400).send("not found");
-    }
-
-    
+app.put('/products/:id', (req, res, next) => {
+    try {
+        let ID = req.params.id;
+        //let j = 0
+        let wanted = products.find((ele) => { return ele.id == ID});//j++; 
+        if (wanted) {
+            let i = products.indexOf(wanted);
+            const data = req.body; //if data contains the whole object
+            // products[wanted] = data; //this does not work //using the object as an index in array of objects
+            if (data.name)
+                products[i].name = data.name; 
+            if (data.price)
+                products[i].price = data.price;
+            res.json(products);
+        } else {
+            // res.status(400).send("not found");
+            const error = new Error("products not found for updating");
+            error.status = 400;
+            next(error);
+        }
+    } catch (error) {
+        next(error);
+    }    
 });
 
 app.delete('/products/:id', (req, res) => {
-    let ID = parseInt(req.params.id);
-    //let j = 0
-    let wanted = products.find((ele) => { return ele.id == ID});//j++; 
-    if (wanted) {
-        let i = products.indexOf(wanted);//you cant compare objects / this works
-        //const data = req.body;
-        products.splice(i, 1);
-        console.log(products);
-        res.json(products);
-    } else {
-        res.status(400).send("not found");
-    }
+    try {
+        let ID = parseInt(req.params.id);
+        //let j = 0
+        let wanted = products.find((ele) => { return ele.id == ID});//j++; 
+        if (wanted) {
+            let i = products.indexOf(wanted);//you cant compare objects / this works
+            //const data = req.body;
+            products.splice(i, 1);
+            console.log(products);
+            res.json(products);
+        } else {
+            //res.status(400).send("not found");
+            const error = new Error("nothing");
+            error.status = 400;
+            next(error);
+        }
+    } catch (error) {
+        next(error);
+    }    
 });
+// function errorHandler(err, req, res, next) {
+//     if (err) {
+//         err.
+//         res.send(err)
+//     }
+// }
+app.use((error, req, res, next) => {
+    // if (error.status != 404)
+    // {
+        console.log(error);
+        res.status(error.status).send(error.message);
+    //}// else {
+    //     // next(error);
+    //     res.status(error.status).send("not found");
+    // }
+})
+app.use((req, res) => { // including the parameters: error and next does not work
+    // if (error) {
+    //     console.log(error);
+    //     res.send(error.message);
 
-app.use((req, res) => {
+    // }
+    //res.status(error.status).send("404 page not found");
     res.status(404).send("404 page not found");
 });
