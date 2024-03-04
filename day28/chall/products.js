668d044dc417699e12ba7a843fd4a4bea940af26
Zizort <mtbox529@gmail.com>
2024-03-05 00:12:42 +0100

    day28 chall

---
+++ day28/chall/products.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {type: String},
+    price: {type: Number, required: true, min: 0},
+    description: {type: String},
+    inStock: {type: Boolean, default: true},
+    createdAt: {type: Date, immutable: true, default: ()=> Date.now()},
+    category: {
+        type: String,
+        enum: ['ab', 'bc', 'cd'], // Predefined values
+        required: true
+     }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
