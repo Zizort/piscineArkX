5815ec005db6fc02602000019b8bee5ef3953bf6
Zizort <mtbox529@gmail.com>
2024-03-31 15:04:49 +0000

    middleware and errorhandling func

--- blogApp/server/controllers/auth.controller.js
+++ blogApp/server/controllers/auth.controller.js
@@ -1,13 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utiles/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     // console.log(req.body);
     try {
         const { username, email, password } = req.body;
 
         if (!username || !email || !password || username === '' || email === '' || password === '') {
-            return res.status(400).json({message: 'all feild arerequired'});
+            // return res.status(400).json({message: 'all feild arerequired'});
+            next(errorHandler(400, 'All feilds are required'));
         }
 
         const hashedPassword = await bcryptjs.hash(password, 10);
@@ -22,6 +24,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json('sign up successful');
     } catch (err) {
-        res.status(500).json({message: err.message});
+        // res.status(500).json({message: err.message});
+        next(err);
     }
 }
