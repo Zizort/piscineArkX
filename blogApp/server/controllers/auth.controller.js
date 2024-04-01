8e0d2767793202d203c57960a95508294f85f174
Zizort <mtbox529@gmail.com>
2024-04-01 10:21:13 +0000

    sign in ui

--- blogApp/server/controllers/auth.controller.js
+++ blogApp/server/controllers/auth.controller.js
@@ -48,6 +48,7 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET,);// {expiresIn: '1d'},);
         //to seperate password from valid user
         const {password: pass, ...rest } = validUser._doc;
+        
         res.status(200).cookie('access_token', token, {
             httpOnly: true,
         }).json(rest);
