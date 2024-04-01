3e6f56e6fedf4b34814968d5b902bf6018393552
Zizort <mtbox529@gmail.com>
2024-04-01 21:53:36 +0000

    headerupdate

--- blogApp/client/src/components/Header.jsx
+++ blogApp/client/src/components/Header.jsx
@@ -5,48 +5,79 @@ import { TextInput } from 'flowbite-react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { Button } from 'flowbite-react'
 import { FaMoon } from 'react-icons/fa'
+import { useSelector } from 'react-redux';
+import { Avatar, Dropdown } from 'flowbite-react';
+
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className='border-b-2'>
-        <Link to='/'className='self-center whitespace-nowrap text-sm sm:text-xl
-        font-semibold dark:text-white'>
-        blog
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white' >stack</span>
-        </Link>
-        <form>
-            <TextInput
-                type='text'
-                placeholder='Search...'
-                rightIcon={AiOutlineSearch}
-                className='hidden lg:inline'
-            />
-        </form>
-        <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-            <AiOutlineSearch />
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Sahand's
+        </span>
+        Blog
+      </Link>
+      <form>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
         </Button>
-        <div className='flex gap-2 md:order-2'>
-          <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-            <FaMoon />
-          </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
           <Link to='/sign-in'>
-            <Button className='' gradientDuoTone='purpleToBlue' outline>
+            <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
             </Button>
           </Link>
-          <Navbar.Toggle/>
-        </div>
-          <Navbar.Collapse> {/*as {div} is added because we cant have 1 ancho inside the other*/}
-            <Navbar.Link active={path === "/"} as={'div'}>
-              <Link to='/'>Home</Link>
-            </Navbar.Link>
-            <Navbar.Link active={path === "/about"} as={'div'}>
-              <Link to='/about'>About</Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
+        )}
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
 
 // export default function Header({theme, isLoggedIn, title, nav1, nav2,nav3}) {
--- blogApp/server/models/user.model.js
+++ blogApp/server/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema( {
     },
     profilePicture:{
         type: String,
-        default: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic-00.iconduck.com%2Fassets.00%2Fprofile-circle-icon-2048x2048-cqe5466q.png&tbnid=rtdN3XGJIJyzHM&vet=12ahUKEwjYk_ast6GFAxXATaQEHUY7DMcQMygAegQIARBx..i&imgrefurl=https%3A%2F%2Ficonduck.com%2Ficons%2F180867%2Fprofile-circle&docid=8qCF3TmdBFRLtM&w=2048&h=2048&q=profile&ved=2ahUKEwjYk_ast6GFAxXATaQEHUY7DMcQMygAegQIARBx",
+        default: "https://www.pngwing.com/en/free-png-zdovr",
     },
     },{timestamps: true}
 );
