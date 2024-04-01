bd06dbfa302a3d6f44fa5b9636fe2212c6819064
Zizort <mtbox529@gmail.com>
2024-04-01 00:24:43 +0000

    sign in rout

--- blogApp/server/controllers/auth.controller.js
+++ blogApp/server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utiles/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     // console.log(req.body);
@@ -26,5 +27,32 @@ export const signup = async (req, res, next) => {
     } catch (err) {
         // res.status(500).json({message: err.message});
         next(err);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password || email === '' || password === '') {
+        return next(errorHandler(400, 'All feilds are required')); //return or no return ?
+    }
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = await bcryptjs.compare(password, validUser.password);
+        if (!validPassword ) { //|| password == validUser.password
+            return next(errorHandler(400, 'User not found'));
+        }
+        //token (cookie) created (encrypted )based on a secret key
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET,);// {expiresIn: '1d'},);
+        //to seperate password from valid user
+        const {password: pass, ...rest } = validUser._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
+
+    } catch (err) {
+        next(err)
     }
 }
--- blogApp/server/routes/auth.route.js
+++ blogApp/server/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';//always add .js
+import { signin, signup } from '../controllers/auth.controller.js';//always add .js
 
 const router = express.Router();
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
 
